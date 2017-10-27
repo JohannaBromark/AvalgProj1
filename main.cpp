ede365997d279a7c2f8ca60ff001b0eaf81197d9
isabellehallman <hallmanisabelle@gmail.com>
2017-10-27 13:52:33 +0200

    Startfil

--- main.cpp
+++ main.cpp
@@ -5,18 +5,13 @@
 #include <math.h>
 #include <tuple>
 
-float calcDistance(float x1, float y1, float x2, float y2){
-    return sqrt(pow((x1-x2), 2)+pow((y1-y2), 2));
-}
-
-
 class City {
     float x, y;
     int index;
 
     public:
         float calcDistance (City&);
-        void City(float inX, float inY, int inIndex){
+        City(float inX, float inY, int inIndex){
             x = inX; y = inY; index = inIndex;
         }
         std::tuple<float, float> getXY(){
@@ -27,7 +22,8 @@ class City {
 
 
 float City::calcDistance (City& otherCity){
-    return 0.0;
+    std::tuple<float, float> otherXY = otherCity.getXY();
+    return sqrt(pow((x-x2), 2)+pow((y-y2), 2));
 };
 
 
