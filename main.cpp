cfe1796ccdb33d26e1d62d84768929262abc2d52
isabellehallman <hallmanisabelle@gmail.com>
2017-10-27 14:22:49 +0200

    Startfil

--- main.cpp
+++ main.cpp
@@ -9,17 +9,29 @@ class City {
     int index;
 
     public:
-        City(float inX, float inY, int inIndex){
-            x = inX; y = inY; index = inIndex;
+        City (float xIn, float yIn, int indexIn) {
+            x = xIn; y = yIn; index = indexIn;
         }
 
-        std::tuple<float, float> getXY(){
+        std::tuple<float, float> getXY() {
             return std::make_tuple(x, y);
         };
 
         float calcDistance (City& otherCity) {
                 return sqrt(pow( ( x - std::get<0>(otherCity.getXY()) ), 2 ) + pow( ( y - std::get<1>(otherCity.getXY()) ), 2 ));
             };
+};
+
+class Savings {
+    float value;
+    City& cityBase, city1, city2; //Referenser för att spara plats (?)
+
+public:
+    Savings (City& cityBaseIn, City& city1In, City& city2In)
+        : cityBase(cityBaseIn), city1(city1In), city2(city2In) //Referenserna måste skapas direkt, görs på detta sätt
+    {
+        value = cityBase.calcDistance(city1) + city2.calcDistance(cityBase) - city2.calcDistance(city1);
+    }
 };
 
 int main() {
