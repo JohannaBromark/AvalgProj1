ac7e85e1ed7274084a82ae080f01535977dfe3d8
JohannaBromark <johanna.bromark@gmail.com>
2017-10-27 13:27:26 +0200

    Commented again

--- main.cpp
+++ main.cpp
@@ -1,9 +1,10 @@
 //Här kommenterar Johanna
+//Här kommenterar Johanna igen
 
 #include <iostream>
 #include <math.h>
 
-float calcDistance(float x1, float y1, float x2, float y2){
+float calcDistance(float x1, fl oat y1, float x2, float y2){
     return sqrt(pow((x1-x2), 2)+pow((y1-y2), 2));
 }
 
