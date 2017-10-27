88abaa87f5affeeb5508903df1f935e73007dd68
isabellehallman <hallmanisabelle@gmail.com>
2017-10-27 14:00:03 +0200

    Startfil

--- main.cpp
+++ main.cpp
@@ -10,21 +10,19 @@ class City {
     int index;
 
     public:
-        float calcDistance (City&);
         City(float inX, float inY, int inIndex){
             x = inX; y = inY; index = inIndex;
         }
+
+        float calcDistance (City& otherCity) {
+            return sqrt(pow( ( x - std::get<0>(otherCity.getXY()) ), 2 ) + pow( ( y - std::get<1>(otherCity.getXY()) ), 2 ));
+        };
+
         std::tuple<float, float> getXY(){
             return std::make_tuple(x, y);
         };
 
 };
-
-
-float City::calcDistance (City& otherCity){
-    return sqrt(pow( ( x - std::get<0>(otherCity.getXY()) ), 2 )+pow( ( y - std::get<1>(otherCity.getXY()) ), 2 ));
-};
-
 
 int main() {
     std::cout << "Hello, World! :)" << std::endl;
