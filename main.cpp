8860b431fc04dc29f2173dc449b9bf62cbbbe01f
JohannaBromark <johanna.bromark@gmail.com>
2017-10-27 13:24:39 +0200

    Added comment to main

--- main.cpp
+++ main.cpp
@@ -1,3 +1,5 @@
+//Här kommenterar Johanna
+
 #include <iostream>
 #include <math.h>
 
@@ -5,14 +7,17 @@ float calcDistance(float x1, float y1, float x2, float y2){
     return sqrt(pow((x1-x2), 2)+pow((y1-y2), 2));
 }
 
+
+
 class city {
 public:
     float calcDistance(city);
 };
 
-float city::calcDistance(<city>& othercity){
 
-}
+float city::calcDistance(<city>& othercity){
+    
+};
 
 int main() {
     std::cout << "Hello, World!" << std::endl;
