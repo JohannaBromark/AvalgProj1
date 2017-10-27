58323a5a53d448f3385f42413736d8e706a3d6c6
isabellehallman <hallmanisabelle@gmail.com>
2017-10-27 13:35:32 +0200

    Startfil

--- main.cpp
+++ main.cpp
@@ -4,19 +4,19 @@
 #include <iostream>
 #include <math.h>
 
-float calcDistance(float x1, fl oat y1, float x2, float y2){
+float calcDistance(float x1, float y1, float x2, float y2){
     return sqrt(pow((x1-x2), 2)+pow((y1-y2), 2));
 }
 
 
-class city {
-public:
-    float calcDistance(city);
+class City {
+    public:
+        float calcDistance (City);
 };
 
 
-float city::calcDistance(<city>& othercity){
-    
+float City::calcDistance (City& otherCity){
+
 };
 
 int main() {
