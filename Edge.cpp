392add9172018ee119c165589109221866c8dcd9
isabellehallman <hallmanisabelle@gmail.com>
2017-10-30 12:08:28 +0100

    Edges

---
+++ Edge.cpp
@@ -0,0 +1,35 @@
+//
+// Created by Isabelle Hallman on 2017-10-30.
+//
+
+#include "Edge.h"
+#include "Node.h"
+#include <iostream>
+
+    Edge::Edge(Node& node1, Node& node2) : to(node1), from(node2) {
+        distance = node1.calcDistance(node2);
+    }
+
+    float Edge::getDistance() {
+        return distance;
+    }
+
+    Node& getToNode() {
+        return to;
+    }
+
+    Node& getFromNode() {
+        return from;
+    }
+
+    bool operator=(Edge& other) {
+        if (to == other.getToNode() && from == other.getFromNode()) {
+            return true;
+        }
+        else if (to == other.getFromNode() && from == other.getToNode()) {
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
---
+++ Edge.h
@@ -0,0 +1,21 @@
+//
+// Created by Isabelle Hallman on 2017-10-30.
+//
+
+#ifndef AVALGPROJ1_LK_H
+#define AVALGPROJ1_LK_H
+#include "Node.h"
+
+class Edge {
+    public:
+        Edge(Node& node1, Node& node2);
+        float getDistance();
+        bool operator=();
+        Node& getToNode(), getFromNode();
+    private:
+        float distance;
+        Node& to, from;
+};
+
+
+#endif //AVALGPROJ1_LK_H
