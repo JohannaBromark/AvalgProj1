40408733a3b9b1b7c1fd517439341b61b4786651
JohannaBromark <johanna.bromark@gmail.com>
2017-10-30 11:01:20 +0100

    Changes to Node

--- Node.cpp
+++ Node.cpp
@@ -1,14 +1,15 @@
 // in node.cpp
 #include "Node.h"
+using namespace std;
 
     Node::Node (float xIn, float yIn, int indexIn) {
         x = xIn; y = yIn; index = indexIn;
     }
 
-    std::tuple<float, float> getXY() {
+    tuple<float, float> Node::getXY() {
         return std::make_tuple(x, y);
     };
 
-    float calcDistance (Node& otherCity) {
+    float Node::calcDistance (Node& otherCity) {
         return sqrt(pow( ( x - std::get<0>(otherCity.getXY()) ), 2 ) + pow( ( y - std::get<1>(otherCity.getXY()) ), 2 ));
     };
