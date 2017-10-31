6378411383c311c9c10d68f1836f658059686c82
jacobbjo <jacob.bjorkman@live.com>
2017-10-31 08:40:51 +0100

    fixing Node.h

--- Node.h
+++ Node.h
@@ -11,8 +11,8 @@ class Node {
         Node(float x, float y, int index);
         float calcDistance (Node& otherCity);        
         std::tuple<float, float> getXY();
+        bool operator==(Node& other);		
         int getIndex();
-        bool operator==(Node& other);
     private:
         float x, y;
         int index;
