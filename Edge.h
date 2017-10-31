f86f64b7debf047167e3282014b6519252085868
isabellehallman <hallmanisabelle@gmail.com>
2017-10-31 15:17:37 +0100

    Justerat edge-klassen så att nodsen den innehåller är pointers

--- Edge.h
+++ Edge.h
@@ -4,20 +4,22 @@
 #include "Node.h"
 
 class Edge {
-    public:
-        Edge(Node& node1, Node& node2);
-        float getDistance() const;
-        bool operator==(Edge& other);
-        bool operator>(Edge& other);
-        bool operator<(Edge& other);
-        Node& getToNode();
-        Node& getFromNode();
+public:
+    Edge(Node node1in, Node node2in);
+    Edge(const Edge& e);
+    float getDistance() const;
+    bool operator==(Edge& other);
+    bool operator>(Edge& other);
+    bool operator<(Edge& other);
+    Node& getNode(int index) const;
 
-    private:
-        float distance;
-        Node& to;
-        Node& from;
+
+private:
+    float distance;
+    Node* node1;
+    Node* node2;
 };
 
 
 #endif //_Edge_h
+
