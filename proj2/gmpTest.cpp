18f2c880f30c845bbc8d87d12640d70c958da011
JohannaBromark <johanna.bromark@gmail.com>
2017-11-29 16:02:17 +0100

    61p på Kattis!!!!

--- proj2/gmpTest.cpp
+++ proj2/gmpTest.cpp
@@ -1,8 +1,6 @@
 #include <iostream>
-//#include <gmp.h>
 #include <gmpxx.h>
 #include <string>
-//#include <cstring>
 #include <vector>
 
 void gFunction(mpz_t& x, const mpz_t& number) {
@@ -46,18 +44,40 @@ int main(){
     mpz_out_str(stdout, 10, d);
     */
     
-    mpz_class a, b, c, d;
+    mpz_t p, answ;
+    mpz_inits(p, answ, NULL);
+    mpz_set_str(p, "13", 10);
+    mpz_class a, b, c, d, j, gcd, h;
+    std::vector<mpz_class> testV;
     
-    a = 2;
+    a = 4;
+    j = 18;
     b = "-3";
     c = a+b;
     d = a/b;
+    testV.push_back(a);
+    testV.push_back(j);
+    h = "123456789012345678900000";
+
 
 
     int e =  mpz_probab_prime_p(a.get_mpz_t(), 1000);
+    int f = mpz_probab_prime_p(p, 1000);
+    mpz_class mod = (4 * 4 + 1) % 7;
+    
+    mpz_gcd(answ, a.get_mpz_t(), j.get_mpz_t());
+    gcd = mpz_class(answ);
+
     std::cout << "summa: " << c << std::endl;
     std::cout << "division: " << d << std::endl;
-    //std::cout << "Is prime? " << e << endl;
-    
+    std::cout << "Is prime? " << e << std::endl;
+    std::cout << "Is prime? " << mpz_probab_prime_p(p, 1000) << std::endl;
+    std::cout << "mod: " << mod << std::endl;
+    std::cout << "gcd: " << gcd << std::endl;
+    std::cout << "längd på lista: " << testV.size()<< std::endl;
+    std::cout << "Jämförelse: " << (a > 1) << std::endl;
+
+    mpz_clear(p);
+    mpz_clear(answ);
     return 0;
 }
